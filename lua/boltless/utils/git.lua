d2dbe9f027b74720cd029208a84f0b4a3878fdc3
BoltlessEngineer <boltlessengineer@gmail.com>
2022-11-09 12:42:29 +0900

    wip: git status

--- lua/boltless/utils/git.lua
+++ lua/boltless/utils/git.lua
@@ -1,12 +1,44 @@
-local fn = vim.fn
-local d = {}
-
-GitStatus = {
+_G.GitStatus = {
   head = '',
   upstream = '',
   ahead = 0,
   behind = 0,
 }
+
+-- TODO: More options are needed. I can commit git changes **inside** neovim
+-- auto refresh git every 2sec (10sec by default & 2sec on neogit enabled maybe)
+local M = {
+  status = {
+    head = '', -- current branch; replaced with `#tag` or `@commit`
+    upstream = '', -- remote tracking branch
+    ahead = 0, -- ahead the remote
+    behind = 0, -- behind the remote
+  }
+}
+function M.get_status()
+  return M.status
+end
+
+function M.update_git_status()
+  local Job = require('plenary.job')
+  Job:new({
+    command = 'git',
+    args = { 'rev-list', '--left-right', '--count', 'HEAD...@{upstream}' },
+    on_exit = function(job, _)
+      local res = job:result()[1]
+      if type(res) ~= 'string' then
+        M.git_status.ahead = 0
+        M.git_status.behind = 0
+        return
+      end
+      local ok, ahead, behind = pcall(string.match, res, '(%d+)%s*(%d+)')
+      if not ok then ahead, behind = 0, 0 end
+      M.git_status.ahead = ahead
+      M.git_status.behind = behind
+    end,
+  }):start()
+end
+
 vim.api.nvim_create_autocmd({ 'VimEnter', 'DirChanged' }, {
   callback = function()
     vim.fn.jobstart({ 'git', 'status', '-b', '--porcelain=v2' }, {
@@ -19,13 +51,13 @@ vim.api.nvim_create_autocmd({ 'VimEnter', 'DirChanged' }, {
           local header, value = line:match('# ([%w%.]+) (.+)')
           if header then
             if header == 'branch.head' then
-              GitStatus.head = value
+              _G.GitStatus.head = value
             elseif header == 'branch.upstream' then
-              GitStatus.upstream = value
+              _G.GitStatus.upstream = value
             elseif header == 'branch.ab' then
               local ahead, behind = value:match('%+(%d+) %-(%d+)')
-              GitStatus.ahead = tonumber(ahead)
-              GitStatus.behind = tonumber(behind)
+              _G.GitStatus.ahead = tonumber(ahead)
+              _G.GitStatus.behind = tonumber(behind)
             end
           end
         end
@@ -33,68 +65,6 @@ vim.api.nvim_create_autocmd({ 'VimEnter', 'DirChanged' }, {
     })
   end
 })
+return M
 -- NOTE: print guide
 -- https://github.com/romkatv/powerlevel10k/blob/master/README.md#what-do-different-symbols-in-git-status-mean
---[[
-local chan = fn.jobstart({ 'git', 'status', '-b', '--porcelain=v2' }, {
-  on_exit = function()
-    P(d)
-  end,
-  on_stdout = function(_, data)
-    if not data then return end
-    if not d.count_staged then
-      d = {
-        count_staged = 0,
-        count_unstaged = 0,
-        count_untracked = 0,
-      }
-    end
-    for _, line in ipairs(data) do
-      if line == '' then return end
-      -- handle line
-      local header, value = line:match('# ([%w%.]+) (.+)')
-      if header then
-        if header == 'branch.head' then
-          d.head = value
-        elseif header == 'branch.upstream' then
-          d.upstream = value
-        elseif header == 'branch.ab' then
-          local ahead, behind = value:match('+(%d+) -(%d+)')
-          d.ahead = ahead
-          d.behind = behind
-        end
-      else
-        local kind, str = line:match('(.) (.+)')
-
-        if kind == '1' then
-          -- changed
-          local mode_staged, mode_unstaged, _, _, _, _, _, _, _ =
-          str:match('(.)(.) (....) (%d+) (%d+) (%d+) (%w+) (%w+) (.+)')
-          if mode_staged ~= '.' then
-            d.count_staged = d.count_staged + 1
-          end
-          if mode_unstaged ~= '.' then
-            d.count_unstaged = d.count_unstaged + 1
-          end
-        elseif kind == '2' then
-          -- renamed/copied
-          local mode_staged, mode_unstaged, _, _, _, _, _, _, _ =
-          str:match('(.)(.) (....) (%d+) (%d+) (%d+) (%w+) (%w+) (%a%d+) (.+)')
-          if mode_staged ~= '.' then
-            d.count_staged = d.count_staged + 1
-          end
-          if mode_unstaged ~= '.' then
-            d.count_unstaged = d.count_unstaged + 1
-          end
-        elseif kind == 'u' then
-          -- unmerged
-          d.count_untracked = d.count_untracked + 1
-        elseif kind == '?' then
-          -- untracked
-          d.count_untracked = d.count_untracked + 1
-        end
-      end
-    end
-  end,
-})
-]]
