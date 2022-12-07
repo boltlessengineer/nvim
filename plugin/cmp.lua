4abd1b2eb5120f9c14893b99d668058557bdb1e5
BoltlessEngineer <boltlessengineer@gmail.com>
2022-12-08 04:38:43 +0900

    fix(UI): better nvim-cmp UI

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -7,10 +7,8 @@ if snip_ok then
   require('luasnip.loaders.from_vscode').lazy_load()
 end
 
--- TODO: snippet help signs (where could I go)
+-- TODO: snippet help signs (show jumpable places)
 
--- TODO: Warn 'Need check nil.' in LuaJIT..? only happens with require'cmp'
--- cmp = cmp or {}
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -19,18 +17,15 @@ cmp.setup {
       end
     end,
   },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
   mapping = cmp.mapping.preset.insert({
     ['<C-k>'] = cmp.mapping.scroll_docs(-1),
     ['<C-j>'] = cmp.mapping.scroll_docs(1),
+    -- TODO: don't select-confirm only when match is already exactly same
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      elseif luasnip.expand_or_jumpable() then
+      elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
@@ -48,26 +43,75 @@ cmp.setup {
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
     { name = 'luasnip' },
   }, {
     { name = 'buffer' },
   }),
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      -- copied from cmp-under-comparator (found in TJ's config)
+      function (entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find("^_+")
+        local _, entry2_under = entry2.completion_item.label:find("^_+")
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
+  window = {
+    completion = cmp.config.window.bordered({
+      border = "none",
+      winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+      col_offset = -3,
+    }),
+    -- documentation = cmp.config.window.bordered(),
+  },
   formatting = {
+    fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       local icons = require 'boltless.ui.icons'
-      vim_item.kind = string.format('%s %s', icons.kind[vim_item.kind], vim_item.kind)
-      vim_item.kind = vim_item.kind
+      vim_item.kind = icons.kind[vim_item.kind]
       vim_item.menu = ({
         nvim_lsp = '[LSP]',
-        nvim_lua = '[lua]',
+        nvim_lua = '[api]',
         luasnip  = '[Snp]',
         buffer   = '[buf]',
+        path     = '[dir]',
+        cmdline  = '[cmd]',
       })[entry.source.name]
+      local function trim(text)
+        -- 10 is for kind & sources width
+        local max = (vim.go.columns / 2) - 10
+        if text and text:len() > max then
+          text = text:match("[^(]+") .. '('
+          if text:len() > max then
+            text = text:sub(1, max) .. "…"
+          end
+        end
+        return text
+      end
+
+      vim_item.abbr = trim(vim_item.abbr)
       return vim_item
     end
   },
 }
+
+-- TODO: show buffer based conmpletions in comment
 
 -- TODO: maybe add cmp_git?
 
@@ -79,11 +123,14 @@ cmp.setup.cmdline({ '/', '?' }, {
   },
 })
 
+-- TODO: show completion after 3 inputs (not when `:wq`)
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
+  completion = {
+    keyword_length = 2,
+  },
+  sources = {
     { name = 'path' },
-  }, {
     { name = 'cmdline' },
-  }),
+  },
 })
