4bcfd84d21373364c22d9fed3f0d6ce253cf53f2
BoltlessEngineer <boltlessengineer@gmail.com>
2023-05-27 19:38:30 +0900

    chore: typo on comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -62,8 +62,8 @@ vim.opt.sessionoptions = { "buffers", "curdir", "folds", "help", "tabpages", "wi
 vim.o.shiftround = true
 vim.o.shiftwidth = 2
 vim.opt.shortmess:append({
-  W = true, -- No splash screen
-  I = true, -- Don't print "written" when editing
+  W = true, -- Don't print "written" when editing
+  I = true, -- No splash screen
   c = true, -- Don't show ins-completion-menu messages (match 1 of 2)
   C = true, -- Don't show messages while scannign ins-completion items (scanning tags)
   s = true, -- Don't show "Search hit BOTTOM" message
