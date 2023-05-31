1719d4518f2b0de3b43558da7b3b5d0753729f36
BoltlessEngineer <boltlessengineer@gmail.com>
2023-06-01 03:19:03 +0900

    feat(mini.ai): add mini.ai

---
+++ lua/plugins/mini_ai.lua
@@ -0,0 +1,39 @@
+return {
+  "echasnovski/mini.ai",
+  keys = {
+    { "a", mode = { "x", "o" } },
+    { "i", mode = { "x", "o" } },
+  },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    init = function()
+      -- PERF: no need to load the plugin because we only need its queries for mini.ai
+      require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+    end,
+  },
+  opts = function()
+    local ai = require("mini.ai")
+    local ts_gen = ai.gen_spec.treesitter
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        -- flip balanced / unbalanced mappings
+        ["("] = { "%b()", "^.().*().$" },
+        [")"] = { "%b()", "^.%s*().-()%s*.$" },
+        ["["] = { "%b[]", "^.().*().$" },
+        ["]"] = { "%b[]", "^.%s*().-()%s*.$" },
+        ["{"] = { "%b{}", "^.().*().$" },
+        ["}"] = { "%b{}", "^.%s*().-()%s*.$" },
+        ["<"] = { "%b<>", "^.().*().$" },
+        [">"] = { "%b<>", "^.%s*().-()%s*.$" },
+        -- remove unused default mappings
+        ["?"] = false,
+        -- custom mappings
+        ["f"] = ts_gen({ a = "@function.outer", i = "@function.inner" }),
+        ["c"] = ts_gen({ a = "@class.outer", i = "@class.inner" }),
+        ["C"] = ts_gen({ a = "@comment.outer", i = "@comment.inner" }),
+      },
+      silent = false,
+    }
+  end,
+}
