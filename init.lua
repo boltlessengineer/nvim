05604cd023006aba79e9c2727fc4348cbeed929f
BoltlessEngineer <boltlessengineer@gmail.com>
2023-09-30 03:26:23 +0900

    feat(abbrevs): add abbrevs

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("config.options")
 require("config.keymaps")
+require("config.abbrevs")
 require("config.autocmds")
 require("config.lazy")
 require("config.ui.statusline")
---
+++ lua/config/abbrevs.lua
@@ -0,0 +1,1 @@
+vim.cmd.cabbrev("neogit", "Neogit")
