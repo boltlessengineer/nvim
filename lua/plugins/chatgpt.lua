2526a857fcfd9499a923416bf5f5f4cf941e677b
Seongmin Lee <boltlessengineer@gmail.com>
2023-11-23 10:05:07 +0000

    fix(chatgpt): proper lazy-loading

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,10 +1,10 @@
 return {
   {
     "robitx/gp.nvim",
+    cmd = { "GpChatNew", "GpChatResponse" },
     -- TODO: add command to read query-selected comment(using power of tree-sitter)
     -- `inoremap <c-g>` will run comment(or single line) above
     keys = {
-      { "<c-g>", desc = "+chatgpt" },
       { "<c-g>n", "<cmd>tabnew|GpChatNew<cr>", desc = "New Chat" },
     },
     config = true,
