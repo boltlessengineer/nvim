6993e2cbb91deddfaaa5e6d0cda0650748f53d13
Seongmin Lee <boltlessengineer@gmail.com>
2024-01-15 16:04:17 +0000

    chore: update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin", "habamax" } },
+  install = { colorscheme = { "kanagawa" } },
   checker = {
     -- automatically check for plugin updates
     enabled = true,
