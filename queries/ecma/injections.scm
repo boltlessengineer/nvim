a7c4d60890d5d60fd9319e7ea6f0c5376c302576
Seongmin Lee <boltlessengineer@gmail.com>
2024-01-28 09:54:33 +0000

    feat(query): add ecmascript injection query

---
+++ queries/ecma/injections.scm
@@ -0,0 +1,5 @@
+((template_string) @injection.content
+  ;; TODO: \s pattern on #match? doesn't work for some reason
+  (#lua-match? @injection.content "^`%s*;+%s?query")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "query"))
