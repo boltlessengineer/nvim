38bd304240b7d0936c677422734a9efebe4b267e
Seongmin Lee <boltlessengineer@gmail.com>
2024-02-10 03:29:25 +0000

    fix(query): set `extend` to not overwrite existing queries

--- queries/ecma/injections.scm
+++ queries/ecma/injections.scm
@@ -1,5 +1,7 @@
+; extends
 ((template_string) @injection.content
   ;; TODO: \s pattern on #match? doesn't work for some reason
   (#lua-match? @injection.content "^`%s*;+%s?query")
   (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
   (#set! injection.language "query"))
