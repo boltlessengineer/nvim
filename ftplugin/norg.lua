f6e0cc032b0da5390d79b030dec706cd982c6fb2
Seongmin Lee <boltlessengineer@gmail.com>
2024-02-19 15:56:02 +0000

    feat: update norg support

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -1,1 +1,3 @@
+-- TODO: maybe simple vim file is enough
 vim.opt_local.conceallevel = 2
+vim.opt_local.formatoptions:remove('r')
--- init.lua
+++ init.lua
@@ -23,5 +23,22 @@ require("config.ui.winbar")
 vim.api.nvim_exec_autocmds("User", { pattern = "InitDone" })
 
 vim.opt.rtp:prepend(vim.fs.normalize("$HOME/projects/tree-sitter-norg3"))
-vim.treesitter.language.add("norg", { path = vim.fs.normalize("$HOME/.cache/tree-sitter/lib/norg.so") })
-vim.treesitter.language.register("norg", "norg")
+if not vim.treesitter.language.get_lang("norg") then
+    vim.treesitter.language.add("norg", { path = vim.fs.normalize("$HOME/.cache/tree-sitter/lib/norg.so") })
+    vim.treesitter.language.register("norg", "norg")
+end
+-- rainbow heading
+vim.cmd[[
+hi link @markup.heading.1.norg @attribute
+hi link @markup.heading.2.norg @label
+hi link @markup.heading.3.norg @constant
+hi link @markup.heading.4.norg @string
+hi link @markup.heading.5.norg @label
+hi link @markup.heading.6.norg @constructor
+hi link @markup.heading.1.marker.norg @attribute
+hi link @markup.heading.2.marker.norg @label
+hi link @markup.heading.3.marker.norg @constant
+hi link @markup.heading.4.marker.norg @string
+hi link @markup.heading.5.marker.norg @label
+hi link @markup.heading.6.marker.norg @constructor
+]]
