1a3c7ae361cf91c7978f0f366a4c80a9479ba06f
BoltlessEngineer <boltlessengineer@gmail.com>
2024-04-19 04:03:13 +0900

    chore: update

--- queries/ecma/injections.scm
+++ queries/ecma/injections.scm
@@ -5,3 +5,12 @@
   (#offset! @injection.content 0 1 0 -1)
   (#set! injection.include-children)
   (#set! injection.language "query"))
+
+( (comment) @comment
+  . (template_string) @injection.content
+  (#match? comment "/\/\* html \*\//")
+  (#offset! @comment 0 3 0 -3)
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "html"))
+  ; (#set-lang-from-info-string! @comment))
