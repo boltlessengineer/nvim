607ee0c9412be67ba127a4d50ee722be578b5d9f
BoltlessEngineer <boltlessengineer@gmail.com>
2024-05-20 00:03:21 +0900

    fix: `nvim-colorizer` -> `nvim-highlight-colors`

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -30,29 +30,14 @@ return {
       apple_simulator = true,
     },
   },
-  -- colorizer
   {
-    "NvChad/nvim-colorizer.lua",
+    "brenoprata10/nvim-highlight-colors",
     event = "BufReadPre",
     opts = {
-      filetypes = { "*", "!lazy" },
-      buftypes = { "*", "!prompt", "!nofile", "!terminal" },
-      user_default_options = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = false, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        AARRGGBB = false, -- 0xAARRGGBB hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes: foreground, background
-        -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = "background", -- Set the display mode.
-        virtualtext = "■",
-      },
-    },
+      render = "virtual",
+      enable_named_colors = true,
+      enable_tailwind = true,
+    }
   },
   -- show status in discord
   {
